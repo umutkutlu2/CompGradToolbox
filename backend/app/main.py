2d6daab00ae2dc4d9c0ed0a357dc57e5dc452e2d
Abdul Halim Al Safadi <abeddsafadi@gmail.com>
2025-11-13 16:27:00 +0300

    Algorithm Addition

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.routes import algorithm
+from app.routes import algorithm_excel
 
 app = FastAPI()
+
+app.include_router(algorithm.router, prefix="/api", tags=["Algorithm"])
+app.include_router(algorithm_excel.router, prefix="/api", tags=["Algorithm, Excel"])
+
+
+
 
 @app.get("/")
 def root():
